f41e49457b7357b85f12a2da173b963edfd7d73b
Rohan Das <Rohan98das@gmail.com>
2025-06-24 04:16:08 +0530

    2 change

--- App.js
+++ App.js
@@ -27,8 +27,8 @@ const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_SERVER,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
+    // database: process.env.DB_DATABASE,
+    // port: process.env.DB_PORT,
     options: {
         encrypt: true,
         trustServerCertificate: true
